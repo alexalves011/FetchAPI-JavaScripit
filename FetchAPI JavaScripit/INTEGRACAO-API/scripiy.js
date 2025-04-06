85d7a5938c8f451d5804d9eda9af019bbbefd4a7
Alex Alves da Silva <167913877+alexalves011@users.noreply.github.com>
2025-04-06 18:22:22 -0300

    Add files via upload

---
+++ FetchAPI JavaScripit/INTEGRACAO-API/scripiy.js
@@ -0,0 +1,26 @@
+document.getElementById('fetch-users').addEventListener('click', fetchUsers);
+
+function fetchUsers() {
+    const userList = document.getElementById('user-list');
+    userList.innerHTML = 'Carregando...'; 
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => {
+            userList.innerHTML = '';
+            data.forEach(user => {
+                const userDiv = document.createElement('div');
+                userDiv.innerHTML = `
+                    <h3>${user.name}</h3>
+                    <p><strong>Email:</strong> ${user.email}</p>
+                    <p><strong>Telefone:</strong> ${user.phone}</p>
+                    <p><strong>Site:</strong> <a href="http://${user.website}" target="_blank">${user.website}</a></p>
+                `;
+                userList.appendChild(userDiv);
+            });
+        })
+        .catch(error => {
+            console.error('Erro buscar Usuario', error);
+            userList.innerHTML = 'Ocorreu um erro ao buscar os usuarios';
+        });
+}
